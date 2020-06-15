830df084a7ce275a65944694c058985bc728de0d
남연우 <yeonu9464@gmail.com>
2020-06-15 15:39:56 +0900

    2020/06/15
    -플레이어 무브
    -플레이어 1인칭, 3인칭
    -플레이어 회전 및 이동(캐릭터 컨트롤러 사용)

---
+++ Unity_FPSproject/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    //카메라가 플레이어를 따라다니기
+    //플레이어한테 바로 카메라를 붙여서 이동해도 상관없다
+    //하지만 게임에 따라서 드라마틱한 연출이 필요한 경우에
+    //타게을 따라다니도록 하는게 1인칭에서 3인칭으로 또는 그 반대로 변경이쉽다
+    //또한 순간이동이 아닌 슈팅게임에서 꼬랑지가 따라다니는것같은 효과도 연출이 가능하다
+    //지금은 우리 눈 역할을 할거라서 그냥 순간이동 시킨다
+
+    public Transform target;
+    public float followSpeed = 10.0f;
+
+    public Transform point3;
+    bool isChange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //카메라 위치를 강제로 타겟위치에 고정해둔다
+        //transform.position = target.position;
+
+        FollowTarget();
+        SwitchSight();
+    }
+
+    private void SwitchSight()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            isChange = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            isChange = false;
+        }
+    }
+
+    private void FollowTarget()
+    {
+        if (isChange == true)
+        {
+            //타겟방향 구하기 (벡터의 뺄셈)
+            //방향 = 타겟 - 자기자신
+            Vector3 dir = target.position - transform.position;
+            dir.Normalize();
+            transform.Translate(dir * followSpeed * Time.deltaTime);
+
+            //문제점 : 타겟에 도착하면 덜덜덜 거림
+            if (Vector3.Distance(transform.position, target.position) < 1.0f)
+            {
+                transform.position = target.position;
+            }
+        }
+        else
+        {
+            Vector3 dir2 = point3.position - transform.position;
+            dir2.Normalize();
+            transform.Translate(dir2 * followSpeed * Time.deltaTime);
+
+            //문제점 : 타겟에 도착하면 덜덜덜 거림
+            if (Vector3.Distance(transform.position, point3.position) < 1.0f)
+            {
+                transform.position = point3.position;
+            }
+        }
+
+    }
+}
---
+++ Unity_FPSproject/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    public float speed = 5.0f;
+
+    CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir.Normalize();    //대각선이동 속도를 상하좌우속도와 동일하게 만들기
+        //게임에 따라 일부로 대각선은 빠르게 이동하도로고 하는 경우도 있다
+        //이럴때는 벡터의 정규화(노말라이즈)를 하면 안된다
+        //transform.Translate(dir * speed * Time.deltaTime);
+
+        //카메라가 보는 방향으로 이동해야 한다
+        dir = Camera.main.transform.TransformDirection(dir);
+        //transform.Translate(dir * speed * Time.deltaTime);
+
+        //심각한 문제 : 하늘 날라다님, 땅 뚫음, 충돌처리 안됨
+        //캐릭터컨트롤러 컴포넌트를 사용한다!!
+        //캐릭터컨트롤러는 충돌감지만 되고 물리가 적용안된다
+        //따라서 충돌감지를 하기 위해서는 반드시
+        //
+        cc.Move(dir * speed * Time.deltaTime);
+
+    }
+}
